f3d3a05f226b90cb1c3df3ca467736324938ccfd
sy <shi06yue@gmail.com>
2020-05-19 09:13:17 +0800

    分页

--- teacherInfo.py
+++ teacherInfo.py
@@ -6,7 +6,8 @@ import json
 from requests.packages import urllib3
 
 # 教师 openID
-openId = ""
+openId = "oWRkU0cTJcXzfrWgG1XCnNxWZG8E"
+
 
 headers = {
     "Host": "a.welife001.com",
@@ -16,10 +17,17 @@ headers = {
     "imprint": openId,
     "Accept-Encoding": "gzip, deflate, br"
 }
+page = 0
+while True:
+    urllib3.disable_warnings()
+    r = requests.get("https://a.welife001.com/info/getTeacherInfo?openid="+openId+"&type=-1&onlyMe=false&page="+str(page)+"&time=-1&lookAll=true&cls=-1&size=10&teacher_cate=teach_class_list&role_detail_id=undefined&nv=1",headers=headers,verify=False)
+    data = r.text
+    data = json.loads(data)
+    if len(data['result']) == 0:
+        break
+    print(data['result'])
+    page = page + 1
 
-urllib3.disable_warnings()
-r = requests.get("https://a.welife001.com/info/getTeacherInfo?openid="+openId+"&type=-1&onlyMe=false&page=0&time=-1&lookAll=true&cls=-1&size=10&teacher_cate=teach_class_list&role_detail_id=undefined&nv=1",headers=headers,verify=False)
 
-print(r.text)
 
 
